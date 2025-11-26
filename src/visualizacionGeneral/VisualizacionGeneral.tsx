d891b0929d3739a72e69d5c3013d1629de398b97
oscar <oscarmancilla227@gmail.com>
2025-11-26 14:36:31 -0600

    add: visualizacion de estatusRatio general

--- src/visualizacionGeneral/VisualizacionGeneral.tsx
+++ src/visualizacionGeneral/VisualizacionGeneral.tsx
@@ -2,8 +2,33 @@ import React, { useEffect, useState } from "react";
 import { Plan } from "./components/Plan";
 import PlanSquare from "./components/PlanSquare";
 import { HeaderTurno } from "@/components/myComponents/HeaderTurno";
+import axios from "axios";
 
 const VisualizacionGeneral = () => {
+  const [estatus, setEstatus] = useState(null);
+  const [color, setColor] = useState("");
+
+  const obtenerEstatus = async () => {
+    const response = await axios.get(
+      "http://localhost:3000/api/estatus/obtenerEstatusRatio",
+    );
+
+    console.log(response.data.response);
+
+    setEstatus(response.data.response[0].nombre);
+    setColor(response.data.response[0].color);
+  };
+
+  useEffect(() => {
+    obtenerEstatus();
+    const loop = setInterval(() => {
+      obtenerEstatus();
+      console.log("Loop iniciado");
+    }, 1000);
+
+    return () => clearInterval(loop);
+  }, []);
+
   return (
     <div>
       <HeaderTurno turno="Primer turno"></HeaderTurno>
@@ -60,6 +85,15 @@ const VisualizacionGeneral = () => {
         <PlanSquare numero={"00.00"} color="#49FF00"></PlanSquare>
         <PlanSquare numero={"00.00"} color="#49FF00"></PlanSquare>
         <PlanSquare numero={"00.00"} color="#49FF00"></PlanSquare>
+      </div>
+
+      <div
+        className="m-2 rounded-2xl border border-white p-5"
+        style={{ background: color }}
+      >
+        <h2 className="flex justify-center p-5 text-5xl font-bold text-white">
+          {estatus}
+        </h2>
       </div>
     </div>
   );
